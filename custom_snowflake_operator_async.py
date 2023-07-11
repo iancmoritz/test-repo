df1758264ea9aff467b3addcb7b8c987d428789e
Ian Moritz <ian@alkeri.com>
2023-07-11 15:52:19 -0400

    Update custom_snowflake_operator_async.py

--- custom_snowflake_operator_async.py
+++ custom_snowflake_operator_async.py
@@ -11,6 +11,14 @@ class SnowflakeAuditLogOperator(BaseOperator):
 
     ui_color = "#e8f7e4"
     inherits_from_empty_operator = True
+    def __init__(
+        self,
+        *,
+        sql: str
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.sql = sql
 
-    def execute(self, context: Context, sql: str):
-        print(sql)
+    def execute(self, context: Context):
+        print(self.sql)
